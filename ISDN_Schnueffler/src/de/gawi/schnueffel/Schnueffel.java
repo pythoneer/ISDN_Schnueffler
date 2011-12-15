b28e5b062ff518c81f736d918b4031e6de007588
Dustin <dustin.bensing@googlemail.com>
2011-12-15 16:19:55 +0100

    init

---
+++ ISDN_Schnueffler/src/de/gawi/schnueffel/Schnueffel.java
@@ -0,0 +1,15 @@
+package de.gawi.schnueffel;
+
+public class Schnueffel 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
